9f6932e45bd61a388e4f9f280d19caa336163e5e
Carlos Alexandre <carlos11alexandre11@gmail.com>
2021-03-26 11:49:50 -0300

    .

--- config/database.js
+++ config/database.js
@@ -39,9 +39,7 @@ module.exports = {
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1)),
-      extra: {
-        ssl: true
-      }
+      ssl: true
     },
     debug: Env.get('DB_DEBUG', false)
   }
